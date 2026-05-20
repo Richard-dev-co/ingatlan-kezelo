2ec467b470d163f4f2a402e5be99ff378b5ecd6b
Richard-dev-co <bodor.richard@gmail.com>
2026-05-20 14:30:42 +0200

    berlok js javítva

--- berlok.js
+++ berlok.js
@@ -1,8 +1,6 @@
 const supabaseUrl = 'https://hbhugixkxwzelzonwmnr.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiaHVnaXhreHd6ZWx6b253bW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwODQ1NzMsImV4cCI6MjA5NDY2MDU3M30.DpLzLUPITaABJdxeZx1GP9M2cTzf4tIdCsDZQV-RYSg'
-
-const { createClient } = supabase
-const client = createClient(supabaseUrl, supabaseKey)
+const client = window.supabase.createClient(supabaseUrl, supabaseKey)
 
 async function bejelentkezesEllenorzese() {
     const { data: { session } } = await client.auth.getSession()
