738861325d03465d7042a8a936da51583b99b80e
Richard-dev-co <bodor.richard@gmail.com>
2026-05-21 14:21:26 +0200

    foglalások szerkesztése javítva

--- foglalasok.js
+++ foglalasok.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'IDE_A_TE_PROJECT_URL-ED'
-const supabaseKey = 'IDE_A_TE_ANON_KEY-ED'
+const supabaseUrl = 'https://hbhugixkxwzelzonwmnr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiaHVnaXhreHd6ZWx6b253bW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwODQ1NzMsImV4cCI6MjA5NDY2MDU3M30.DpLzLUPITaABJdxeZx1GP9M2cTzf4tIdCsDZQV-RYSg'
 const client = window.supabase.createClient(supabaseUrl, supabaseKey)
 
 async function bejelentkezesEllenorzese() {
