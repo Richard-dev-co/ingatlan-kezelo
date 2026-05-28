a29e7eeaa549d92dc39caff362f21786e6c22cea
Richard-dev-co <bodor.richard@gmail.com>
2026-05-28 09:13:46 +0200

    consol log 2

--- app.js
+++ app.js
@@ -121,7 +121,14 @@ async function torles(id) {
 async function fajlTorles(bucket, utvonal) {
     if (!confirm('Biztosan törölni szeretnéd ezt a fájlt?')) return
     
-    console.log('Törlés:', bucket, utvonal)
+   console.log('Törlés:', bucket, utvonal)
+    
+    const result = await client.storage
+        .from(bucket)
+        .remove([utvonal])
+    
+    console.log('Eredmény:', result)
+
     
     const { error } = await client.storage
         .from(bucket)
